4a65c0110317972306d75c1fe6a3af5a33cc40bd
Naoki Haba <naosprintrunner6385@gmail.com>
2023-08-12 02:00:59 +0900

    feat: 編集・更新・削除API実装

---
+++ app/notes/[id]/edit/page.tsx
@@ -0,0 +1,21 @@
+import { getNote } from "@/app/notes/[id]/getNote";
+import { Metadata } from "next/types";
+import Link from "next/link";
+import EditNote from "@/app/notes/[id]/edit/EditNote";
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+    const note = await getNote(params.id);
+    return { title: note.title }
+}
+
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const note = await getNote(params.id);
+    return (
+        <main className="mx-2 sm:mx-4">
+            <Link href={`/notes/${params.id}`} className='inline-block focus-visible:ring ring-pink-300 text-gray-500 hover:text-pink-500 active:text-pink-600 text-s md:text-base font-semibold rounded-lg outline-none transition duration-100'>← back</Link>
+            <h2 className='my-4 text-gray-400 text-xs'>Edit Note</h2>
+            <EditNote item={note} />
+        </main>
+    )
+}
